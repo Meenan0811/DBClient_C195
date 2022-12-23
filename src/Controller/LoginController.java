d6def29781402f0117ec7a472b2d911d6c4c8965
Meenan0811 <meenan@mac.com>
2022-12-23 11:15:19 -0700

    Added Login and Main FXMl files

---
+++ src/Controller/LoginController.java
@@ -0,0 +1,30 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+    @FXML
+    private TextField userText;
+    @FXML
+    private TextField passText;
+    @FXML
+    private Label userNameLabel;
+    @FXML
+    private Label passLabel;
+    @FXML
+    private Label locLabel;
+    @FXML
+    private Label locTagLabel;
+    @FXML
+    private Button submitButton;
+
+
+
+    public void setSubmitButton(javafx.event.ActionEvent actionEvent) {
+    }
+
+    //FIXME Determine user Locale and display
+}
---
+++ src/Main/Main.java
@@ -0,0 +1,37 @@
+package Main;
+
+import helper.JDBC;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Main extends Application {
+
+
+
+    /*@Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource(""));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }*/
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../FXML/login.fxml")));
+        primaryStage.setTitle("Login");
+        primaryStage.setScene(new Scene(root, 569, 385));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+        //JDBC.openConnection();
+
+    }
+}
